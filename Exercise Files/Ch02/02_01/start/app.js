a99393b88a311653999b51602626f04514191a00
Jesse Tapia <jtapia21@uic.edu>
2018-05-02 01:10:28 +0000

    finished ch2.1

--- Exercise Files/Ch02/02_01/start/app.js
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -1,6 +1,13 @@
 (function() {
-  "use strict";
+  "use strict"; //Avoid errors
 
   // Start here
+  var ProductCustomizer = React.createElement(
+    "div", 
+    { className: "customizer" }, 
+    "Product customizer will go here"
+  );
+  
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 
 })();
